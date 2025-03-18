f19de15483343a1ccb2f17eeff0db5003f22f532
Zachary <zef20@email.vccs.edu>
2025-03-18 11:34:28 -0400

    Removed code from init.sh for now. Updated .yaml to test without database. (Commented out code)

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,6 @@
 # First need to make sure django is installed so the code can be read Django
-pip install Django
+
+
 
 # Then run a command to start the Django server
 # Then run a command to start the React server
